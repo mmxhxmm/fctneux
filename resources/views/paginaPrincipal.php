0f59d7ecd9a7f5be45828edbca0cc83480cec0ad
Adrian <adrian.gofa@gmail.com>
2025-02-03 16:54:05 +0100

    1

---
+++ resources/views/paginaPrincipal.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina principal</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo"></div>
+            <div class="usuario">
+                <ul>
+                    <li>Perfil</li>
+                    <li>Ver Coordinadores</li>
+                    <li>Ver Registradores</li>
+                    <li>Logout</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>    
+        <section class="portada">
+            <div class="bloque">
+                <div>Acceso area de trabajo.</div>
+                <div>Tu nivel de acceso podria verse limitado a tu rango dentro de la empresa.</div>
+            </div>
+        </section>
+        <section class="botones">
+            <div class="botonEmpresas">
+                <div class="texto">Acceso a Plataforma de</div>
+                <div class="nombre">EMPRESAS</div>
+                <div class="botonNaranja">
+                    <div class="flechaIzq"></div>
+                </div>
+            </div>
+            <div class="botonTareas">
+            <div class="texto">Acceso a Plataforma de</div>
+                <div class="nombre">TAREAS</div>
+                <div class="botonAzul">
+                    <div class="flechaDer"></div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div></div>
+    </footer>
+</body>
+</html>
