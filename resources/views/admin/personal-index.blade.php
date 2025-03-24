2ebda18b60f035f8c83dba8106ff6b326fda7bcc
mmxhxmm <mahammalikk1211@gmail.com>
2025-03-24 19:36:26 +0100

    added pop-up de usuarios

--- resources/views/admin/personal-index.blade.php
+++ resources/views/admin/personal-index.blade.php
@@ -8,16 +8,113 @@
             <!-- Main Heading and Buttons -->
             <div class="bg-black_transp h-16 relative flex justify-between items-center px-10 w-full">
                 <!-- Left buttons (Añadir and Eliminar) -->
-                <div class="flex items-center space-x-4">
+                <div class="flex items-center">
                     <!-- Añadir button -->
                     <div class="w-[110px] h-10">
-                        <div class="bg-[#ff8300] rounded-[100px] w-[110px] h-10 flex justify-center items-center">
-                            <a href="{{ route('personal-form') }}">
-                                <p class="text-white font-roboto text-base font-bold">+ Añadir</p>
-                            </a>
-                        </div>
-                    </div>
+                <div class="bg-[#ff8300] rounded-[100px] w-[110px] h-10 flex justify-center items-center">
+                    <button id="openModal" class="text-white font-roboto text-base font-bold">+ Añadir</button>
+                </div>
+            </div>
 
+            <!-- The Modal -->
+            <div id="myModal" class="fixed inset-0 flex bg-black bg-opacity-50 hidden justify-center items-center z-50">
+                <div class="bg-white border-2 border-blue w-[60%] h-auto rounded-lg">
+                    <div class="relative">
+                        <div class="bg-blue text-white text-center p-2 text-four">Datos de Usuarios</div>
+                        <div class="p-16 text-blue flex justify-center items-center">
+                            <form action="" class="w-full max-w-3xl">
+                                <!-- First Row -->
+                                <div class="flex gap-6 mb-4 justify-center">
+                                    <div class="flex-1">
+                                        <label for="nif-personal" class="block w-[200px] pb-3">NIF</label>
+                                        <input type="text" name="nif-personal" maxlength="9" required class="ml-2 rounded-lg w-[70%] border-2 border-blue">
+                                    </div>
+                                    <div class="flex-1">
+                                        <label for="nivel-personal" class="block w-[200px] pb-3">Nivel de acceso</label>
+                                        <select name="nivel-personal" id="nivel-personal" class="ml-2 rounded-lg w-[70%] border-2 border-blue">
+                                            <option value="Coodinador">Coordinador</option>
+                                            <option value="Registrador">Registrador</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <!-- Second Row -->
+                                <div class="flex gap-6 mb-4 justify-center">
+                                    <div class="flex-1">
+                                        <label for="nombre-personal" class="block w-[200px] pb-3">Nombre</label>
+                                        <input type="text" name="nombre-personal" required class="ml-2 rounded-lg w-[70%] border-2 border-blue">
+                                    </div>
+                                    <div class="flex-1">
+                                        <label for="apellido-personal" class="block w-[200px] pb-3">Apellido</label>
+                                        <input type="text" name="apellido-personal" class="ml-2 rounded-lg w-[70%] border-2 border-blue">
+                                    </div>
+                                </div>
+
+                                <!-- Third Row -->
+                                <div class="flex gap-6 mb-4 justify-center">
+                                    <div class="flex-1">
+                                        <label for="correo-personal" class="block w-[200px] pb-3">Correo Electronico</label>
+                                        <input type="email" name="correo-personal" class="ml-2 rounded-lg w-[70%] border-2 border-blue">
+                                    </div>
+                                    <div class="flex-1">
+                                        <label for="tel-personal" class="block w-[200px] pb-3">Teléfono</label>
+                                        <input type="tel" name="tel-personal" maxlength="9" class="ml-2 rounded-lg w-[70%] border-2 border-blue">
+                                    </div>
+                                </div>
+
+                                <!-- Fourth Row -->
+                                <div class="flex gap-6 mb-4 justify-center">
+                                    <div class="flex-1">
+                                        <label for="municipio-personal" class="block w-[200px] pb-3">Municipio</label>
+                                        <input type="text" name="municipio-personal" class="ml-2 rounded-lg w-[70%] border-2 border-blue">
+                                    </div>
+                                    <div class="flex-1">
+                                        <label for="situacion-personal" class="block w-[200px] pb-3">Situación</label>
+                                        <select name="situacion-personal" id="situacion-personal" class="ml-2 rounded-lg w-auto mb-4 border-2 border-blue">
+                                            <option value="Alta">Alta</option>
+                                            <option value="Baja">Baja</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <!-- Buttons Section -->
+                                <div class="flex gap-6 mt-4 justify-end">
+                                    <button type="submit" class="bg-blue text-white p-3 px-6 rounded-lg">Guardar</button>
+                                    <button type="button" class="bg-orange text-white p-3 px-6 rounded-lg">Eliminar</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <!-- Close Button -->
+                         
+                        <button id="closeModal" class="absolute top-0 w-10 h-10 right-0 bg-orange text-white rounded-[100px] m-2">X</button>
+                    </div>
+                </div>
+            </div>
+                <script>
+                    // Get the modal, open button, and close button
+                            const modal = document.getElementById("myModal");
+                            const openModalBtn = document.getElementById("openModal");
+                            const closeModalBtn = document.getElementById("closeModal");
+
+                            // Open the modal
+                            openModalBtn.onclick = function() {
+                                modal.classList.remove("hidden"); // Show the modal
+                            };
+
+                            // Close the modal
+                            closeModalBtn.onclick = function() {
+                                modal.classList.add("hidden"); // Hide the modal
+                            };
+
+                            // Close the modal if clicked outside the modal content
+                            window.onclick = function(event) {
+                                if (event.target === modal) {
+                                    modal.classList.add("hidden"); // Hide the modal if clicked outside
+                                }
+                            };
+
+                </script>
                     <!-- Eliminar button -->
                     <!-- <div class="w-[110px] h-10">
                         <div class="bg-[#002f86] rounded-[100px] w-[110px] h-10 flex justify-center items-center">
@@ -71,7 +168,7 @@
                 </div>
                 <div class="absolute right-0 top-[10em] animate-right">
                     <a href="{{ $state == 'activo' ? 'personal-no-activo' : 'personal-activo' }}" class="hover:text-white hover:border-none justify-end px-4 rounded-tl-[0px] rounded-tl-[50px] rounded-bl-[50px] rounded-bl-[0px] px-4 bg-blue w-[220px] h-[40px] opacity-90 text-[16px] text-white text-left font-roboto flex-grow-0 flex justify-end items-center">
-                        Usuarios {{ $state == 'Activo' ? 'activos' : 'No Activos'}} >>>
+                        Usuarios {{ $state == 'activo' ? 'No Activos' : 'activos'}} >>>
                     </a>
                 </div>
 
