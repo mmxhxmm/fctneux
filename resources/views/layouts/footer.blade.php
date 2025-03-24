78a2be024b422b086539e74408aa4acf789dada4
Alex <alex.vadmik@gmail.com>
2025-03-24 01:24:49 +0100

    fix: More link route fixes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="bg-primary h-17 w-full">
     <div class="pt-8 text-center text-white text-sm font-roboto_condensed">
         <div class="flex justify-center gap-x-3 mb-3">
-            <a href="empresa-index" class="flex items-center justify-center gap-x-1 hover:underline focus:outline-none focus:underline" title="Ir a la página de Empresa">
+            <a href="{{ route('empresa-index') }}" class="flex items-center justify-center gap-x-1 hover:underline focus:outline-none focus:underline" title="Ir a la página de Empresa">
                 <img src="{{ asset('images/icons/icons8-business-96.png') }}" alt="" width="20px" class="invert brightness-0">
                 <p>Empresas</p>
             </a>
             <label>-</label>
-            <a href="tareas-index" class="flex items-center justify-center gap-x-1 hover:underline focus:outline-none focus:underline" title="Ir a la página de Tareas">
+            <a href="{{ route('tareas-index') }}" class="flex items-center justify-center gap-x-1 hover:underline focus:outline-none focus:underline" title="Ir a la página de Tareas">
                 <img src="{{ asset('images/icons/icons8-address-book-96.png') }}" alt="" width="20px" class="invert brightness-0">
                 <p>Tareas</p>
             </a>
