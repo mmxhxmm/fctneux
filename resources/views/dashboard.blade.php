cef870b60b0f37ef79513da9747f8a00af9fe390
Alex <alex.vadmik@gmail.com>
2025-04-24 18:44:27 +0200

    feat: Added error page functionality and changed screen height

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,12 @@
 <x-app-layout>
-    <div class="w-full flex flex-col grid-rows-3 bg-white">
+    <div class="h-[80.55vh] w-full flex flex-col grid-rows-3 bg-white">
         <!-- Background Image Section -->
-        <div class="relative h-[250px] flex-grow-0 flex-shrink-0" style="background-image: url('../images/fct-bg.png'); background-size: cover; background-position: center;">
+        <div class="relative h-[300px] flex-grow-0 flex-shrink-0" style="background-image: url('../images/fct-bg.png'); background-size: cover; background-position: center;">
             <!-- Opacity overlay -->
             <div class="absolute inset-0 bg-primary opacity-40"></div>
             
             <!-- Main Heading -->
-            <div class="w-full h-[250px] flex flex-col items-center justify-center relative px-4 fade-in">
+            <div class="w-full h-full flex flex-col items-center justify-center relative px-4 fade-in">
                 <div class="text-center">
                     <p class="text-8xl font-hammersmith text-white" style="text-shadow: 2px 4px 2px rgba(0,0,0,0.40)">
                         FCT<span class="text-orange">Nexus</span>
@@ -16,7 +16,7 @@
         </div>
 
         <!-- Buttons Section -->
-        <div class="w-full relative my-14 sm:h-[18em] flex flex-col sm:flex-row items-start justify-center">
+        <div class="w-full relative my-14 sm:h-[100%] flex flex-col sm:flex-row items-start justify-center">
             <!-- First Button (Acceso a Plataforma) -->
             <div class="relative w-full h-[15em] sm:h-full animate-left">
                 <a href="{{ route('empresa-index') }}">
@@ -27,7 +27,7 @@
                             <p class="mt-4 text-xl font-extrabold relative text-center">EMPRESAS</p>
                         </div>
                         <!-- Arrow cont -->
-                        <div class="absolute pr-6 top-[13.5em] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <div class="absolute pr-6 top-[15.5em] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                             <div class="w-[4.3rem] h-[4.3rem] rounded-full bg-orange flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="-1 0 25 24" class="fill-white_dull scale-x-[-1]">
                                 <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/>
@@ -47,7 +47,7 @@
                             <p class="mt-4 text-xl font-extrabold relative text-center">TAREAS</p>
                     </div>
                     <!-- Arrow cont -->
-                        <div class="absolute ml-3 top-[13.5em] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <div class="absolute ml-3 top-[15.5em] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                             <div class="w-[4.3rem] h-[4.3rem] rounded-full bg-primary flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="-1 0 25 24" class="fill-white_dull">
                                 <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/>
--- resources/views/pages/errors/404.blade.php
+++ resources/views/pages/errors/404.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <section class="bg-white">
-        <div class="flex flex-col items-center justify-center gap-10 p-10">
+        <div class="h-[80.55vh] flex flex-col items-center justify-center gap-10 p-10">
             <img src="../images/errorOscuro.png" alt="" class="w-[50%]">
-            <button class="bg-[#263652] mb-6 text-white font-bold py-2 px-10 shadow-xl hover:size-30 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+            <button onclick="{{ route('dashboard') }}" class="bg-[#263652] mb-6 text-white font-bold py-2 px-10 shadow-xl hover:size-30 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">
                 Regresar
             </button>
         </div>
